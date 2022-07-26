04c387a17634bbb4e143792257d277b81ac05f38
Taiwo Adejare <adejareemma@gmail.com>
2022-07-25 17:23:47 -0700

    fix: better anagram solution

--- solving_patterns/anagrams/solution.js
+++ solving_patterns/anagrams/solution.js
@@ -1,29 +1,21 @@
-function solution(str1, str2) {
+function validAnagram(str1, str2) {
     // Check both array length if they are equal
     if(str1.length !== str2.length) {
         return false
     }
 
     // convert string into frequecy object for each char
-    let str1Freq = {}
-    let str2Freq = {}
+    let lookup = {}
 
     for (let char of str1) {
-        str1Freq[char] = (str1Freq[char] || 0 ) + 1
+        lookup[char] = (lookup[char] || 0 ) + 1
     }
 
     for (let char of str2) {
-        str2Freq[char] = (str2Freq[char] || 0 ) + 1
-    }
-    console.log(str1Freq, str2Freq)
-    for (let char in str1Freq) {
-        // Check if char exist in second string
-        if(!(char in str2Freq)) {
+        if(!lookup[char]) {
             return false
-        }
-        // Check if char account in first string equal second 
-        if(str1Freq[char] !== str2Freq[char]) {
-            return false
+        } else {
+            lookup[char] -= 1
         }
     }
 
@@ -32,4 +24,4 @@ function solution(str1, str2) {
 }
 
 
-console.log(solution('ics', 'sci'));
+console.log(validAnagram('aaz', 'aza'));
